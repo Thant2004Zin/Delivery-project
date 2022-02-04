e2ba15e3187d3f05bb6ec3d98cf3288a24f505b3
Thant2004Zin` <frequency307@gmail.com>
2022-02-04 12:11:56 +0630

    master

---
+++ app.js
@@ -0,0 +1,48 @@
+const myAtropos = Atropos({
+    el: '.my-atropos',
+    // rest of parameters
+});
+const myAtropos2 = Atropos({
+    el: '.my-atropos2',
+    // rest of parameters
+});
+const myAtropos3 = Atropos({
+    el: '.my-atropos3',
+    // rest of parameters
+});
+const myAtropos4 = Atropos({
+    el: '.my-atropos4',
+    // rest of parameters
+});
+const myAtropos5 = Atropos({
+    el: '.my-atropos5',
+    // rest of parameters
+});
+const myAtropos6 = Atropos({
+    el: '.my-atropos6',
+    // rest of parameters
+});
+const myAtropos7= Atropos({
+    el: '.my-atropos7',
+    // rest of parameters
+});
+const myAtropos8 = Atropos({
+    el: '.my-atropos8',
+    // rest of parameters
+});
+const myAtropos9 = Atropos({
+    el: '.my-atropos9',
+    // rest of parameters
+});
+const myAtropos10 = Atropos({
+    el: '.my-atropos10',
+    // rest of parameters
+});
+const myAtropos11 = Atropos({
+    el: '.my-atropos11',
+    // rest of parameters
+});
+const myAtropos12 = Atropos({
+    el: '.my-atropos12',
+    // rest of parameters
+});
